8da34df16370d89e00c94ea9a80a1ffa8b50a5df
BronsonLovesCode <132307562+BronsonLovesCode@users.noreply.github.com>
2023-05-12 10:44:44 +1200

    Created pickup or delivery menu
    Created different versions of menu, refining it in version 4.

---
+++ components/pickup_delivery_menu v1.py
@@ -0,0 +1,16 @@
+# Bugs
+# Will only work for valid input "d" and "p"
+
+# Menu so that users can choose either pickup or delivery
+
+print ("*** Do you want your order delivered or are you picking it up? ***")
+
+print ("*** For delivery, enter d. For pickup, enter p. ***")
+
+delivery = input()
+
+if delivery == "d":
+    print ("Delivery")
+
+elif delivery == "p":
+     print ("Pickup")
---
+++ components/pickup_delivery_menu v3.py
@@ -0,0 +1,29 @@
+# Menu so that users can choose either pickup or delivery
+
+# Bug - Need to make it so that it only accepts 1 or 2
+
+print ("*** Is your order for pickup or delivery? ***")
+
+print ("*** For delivery, enter 1. ***")
+print ("*** For pickup, enter 2. ***")
+
+low = 1
+high = 2
+
+while True:
+      try:
+            delivery = int(input("*** Please enter a number. *** "))
+            if delivery >= 1 and delivery <= 2:
+                  if delivery == 1:
+                        print ("Delivery")
+                        break
+
+                  elif delivery == 2:
+                        print ("Pickup")
+                        break
+            else:
+                  print ("*** The number must be 1 or 2. *** ")
+      except ValueError:
+            print ("*** That was not a valid input... *** ")
+            print ("*** Please enter 1 or 2. *** ")
+          
