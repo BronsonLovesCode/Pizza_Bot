551d8d40bc23e09a45b0bc9db64e45bf147b19ed
BronsonLovesCode <132307562+BronsonLovesCode@users.noreply.github.com>
2023-05-31 15:09:16 +1200

    Printer_order Function
    Added formatting to print_order and added total cost.
    Developed into a function.

--- order_print v2.py
+++ order_print v2.py
@@ -8,11 +8,8 @@ order_cost = [8.50, 8.50, 8.50, 13.50]
 
 cust_details = {'name':'Mark', 'phone':'1221223452', 'house':'45', 'street':'Barry', 'suburb':'Howick'}
 
-
-
-#print("\n",cust_details['name'],"\n",cust_details['phone'],"\n",cust_details['house'],"\n",cust_details['street'],"\n",cust_details['suburb'])
-print("Name: {} \n Phone: {} \n House: {} \n Street: {} \n Suburb: {}".format(cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb']))
-
+print("\n Name: {} \n Phone: {} \n House: {} \n Street: {} \n Suburb: {}".format(cust_details['name'],cust_details['phone'],cust_details['house'],
+    cust_details['street'],cust_details['suburb']))
 
 
 count = 0
